f78aaec6e20d4e7a159cd0a420a6a5ab8846de96
Mohan G <mohanmg553@gmail.com>
2026-02-09 13:40:18 +0530

    Added Swagger UI

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 from typing import List
 
 app = FastAPI(title="Two Sum API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class TwoSumRequest(BaseModel):
     nums: List[int]
